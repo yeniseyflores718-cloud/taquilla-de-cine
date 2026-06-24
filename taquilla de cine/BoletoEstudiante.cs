aec0c567306bf9e383f62ab5dbe67d3cbaac4f30
danielRamos <daniel.r.c.dk23@gmail.com>
2026-06-24 13:47:02 -0600

    SE CREO LA CLASE BOLETO ESTUDIANTE Y SE IMPLEMENTO SU FUNCION EN EL IF DEL FORMULARIO PRINCIPAL

---
+++ taquilla de cine/BoletoEstudiante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taquilla_de_cine
+{
+    internal class BoletoEstudiante:boleto
+    {
+        public string Matricula { get; set; }
+
+        public BoletoEstudiante(float precioBase, string matricula) : base (precioBase)
+        {
+            Matricula = matricula;
+        }
+
+        public override float calcularPagoFinal()
+        {
+            return precioBase * 0.70f;
+        }
+    }
+}
